d748a96fb221803a840d37d09c26d230ce3da3bc
ramiuss <127728569+ramiuss@users.noreply.github.com>
2025-06-25 09:47:44 +0900

    Create restaurant_table.py

---
+++ restaurant_table.py
@@ -0,0 +1,10 @@
+total = 6
+def graph(untabled) :
+  print(f'{untabled}person is remained')
+  for tabled in range(2, min(10, untabled) + 1) :
+    print(f'{tabled}person sitted. {untabled - table}person is remained')
+    graph(untabled - tabled)
+    print(f'Return to {untabled}person
+
+graph(total) 
+    
