62b9deb8cf644430d2f06be37cd7fb95e06aa755
ramiuss <127728569+ramiuss@users.noreply.github.com>
2025-06-25 03:36:09 +0000

    increase 100

---
+++ restaurant_table-04.py
@@ -0,0 +1,13 @@
+total = 100
+
+def graph(untabled, tabled_before) :
+    count = 0
+    for tabled in range(max(2, tabled_before) , min(10, untabled) + 1) :
+        if untabled - tabled == 0 :
+            count += 1
+        else :
+            count += graph(untabled - tabled, tabled)
+    return count
+
+print(graph(total, 0))
+        
